c2b03074a696dac87665bcfe38b38aa429d52511
t-taia <95856208+t-taia@users.noreply.github.com>
2022-08-12 18:16:04 +0200

    weather files updated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Weather from './Weather';
 
-import './App.css';
+import './styles.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello from React</h1>
-        <Weather city="Oslo"/>
-      </header>
+      <Weather />
+      <footer>
+          <a
+            href="https://github.com/t-taia/weather-react-test"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source coded
+          </a>{" "}
+          by taia
+        </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,69 @@
 import React from 'react';
-import axios from 'axios';
-import { Puff } from  'react-loader-spinner'
+import "./Weather.css";
 
-
-export default function Weather (props) {
-    function handleResponse(response) {
-        alert(`The weather in ${response.data.name} is ${response.data.main.temp} C`);
-    }
-    let apiKey = "a634dd1dee9be0dfeff6ce65b2525c84";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-    
-    axios.get(apiUrl).then(handleResponse);
+export default function Weather() {
+    let weatherData = {
+      city: "Paris",
+      temperature: 30,
+      date: "Monday 12:00",
+      description: "Sunny",
+      imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      humidity: 32,
+      wind: 6
+    };
+  
     return (
-        <Puff color="red" height={80} width={80} />
+      <div className="Weather">
+        <form className="mb-3">
+          <div className="searchform">
+            <div className="search">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoComplete="off"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-outline-info"
+              />
+            </div>
+          </div>
+        </form>
+        <div className="overview">
+          <h1>{weatherData.city}</h1>
+          <ul>
+            <li>{weatherData.date}</li>
+            <li>{weatherData.description}</li>
+          </ul>
+        </div>
+        <div className="row">
+          <div className="col-6">
+            <div className="clearfix weather-temperature">
+              <img
+                src={weatherData.imgUrl}
+                alt={weatherData.description}
+                className="float-left"
+              />
+              <div className="float-left">
+                <strong>{weatherData.temperature}</strong>
+                <span className="degree-units">
+                  <a href="/">°C</a> | <a href="/">°F</a>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     );
-}
+  }
+  
