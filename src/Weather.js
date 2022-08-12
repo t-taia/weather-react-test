eee3ba653202aad6d6476ffa59e652af7add1b81
t-taia <95856208+t-taia@users.noreply.github.com>
2022-08-12 15:24:37 +0200

    Files added

---
+++ src/Weather.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import axios from 'axios';
+import { Puff } from  'react-loader-spinner'
+
+
+export default function Weather (props) {
+    function handleResponse(response) {
+        alert(`The weather in ${response.data.name} is ${response.data.main.temp} C`);
+    }
+    let apiKey = "a634dd1dee9be0dfeff6ce65b2525c84";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    
+    axios.get(apiUrl).then(handleResponse);
+    return (
+        <Puff color="#00BFFF" height={80} width={80} />
+    );
+}
